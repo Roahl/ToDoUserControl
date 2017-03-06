29ec96414bf882d60fd504e2ba6933ea22f7d991
Roahl <sapeben@gmail.com>
2017-03-06 09:48:21 +0100

    Add project files.

---
+++ ToDoUserControl/Form1.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace ToDoUserControl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.toDoTable1 = new ToDoUserControl.ToDoTable();
+            this.SuspendLayout();
+            // 
+            // toDoTable1
+            // 
+            this.toDoTable1.Location = new System.Drawing.Point(-2, 0);
+            this.toDoTable1.Name = "toDoTable1";
+            this.toDoTable1.Size = new System.Drawing.Size(600, 500);
+            this.toDoTable1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(607, 497);
+            this.Controls.Add(this.toDoTable1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ToDoTable toDoTable1;
+    }
+}
+
---
+++ ToDoUserControl/ToDoTable.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ToDoUserControl
+{
+    public partial class ToDoTable : UserControl
+    {
+        public ToDoTable()
+        {
+            InitializeComponent();
+        }
+
+        private void AddRow(object sender, EventArgs e)
+        {
+            tableLayoutPanel1.RowCount = tableLayoutPanel1.RowCount + 1;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 20));
+            tableLayoutPanel1.Controls.Add(new CheckBox() { Text = "", Margin = new Padding(8,0,0,0) }, 0, tableLayoutPanel1.RowCount - 1);
+            tableLayoutPanel1.Controls.Add(new TextBox() { Text = "",BackColor= SystemColors.Control, BorderStyle= BorderStyle.None }, 1, tableLayoutPanel1.RowCount - 1);
+        }
+    }
+}

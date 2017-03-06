8b7913a849aaced3e3f1656b70c1a11880b6b15d
Roahl <sapeben@gmail.com>
2017-03-06 09:56:19 +0100

    Last commit 6/3/2017

--- ToDoUserControl/ToDoTable.cs
+++ ToDoUserControl/ToDoTable.cs
@@ -21,8 +21,8 @@ namespace ToDoUserControl
         {
             tableLayoutPanel1.RowCount = tableLayoutPanel1.RowCount + 1;
             tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 20));
-            tableLayoutPanel1.Controls.Add(new CheckBox() { Text = "", Margin = new Padding(8,0,0,0) }, 0, tableLayoutPanel1.RowCount - 1);
-            tableLayoutPanel1.Controls.Add(new TextBox() { Text = "",BackColor= SystemColors.Control, BorderStyle= BorderStyle.None }, 1, tableLayoutPanel1.RowCount - 1);
+            tableLayoutPanel1.Controls.Add(new CheckBox() { Text = "", Margin = new Padding(8, 0, 0, 0) }, 0, tableLayoutPanel1.RowCount - 1);
+            tableLayoutPanel1.Controls.Add(new TextBox() { Text = "", BackColor = SystemColors.Control, BorderStyle = BorderStyle.None }, 1, tableLayoutPanel1.RowCount - 1);
         }
     }
 }

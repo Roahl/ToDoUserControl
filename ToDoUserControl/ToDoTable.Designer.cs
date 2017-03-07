63c50cf5a5e0e57e91efc6e9c04c6b1d690f9e07
Roahl <sapeben@gmail.com>
2017-03-07 08:55:36 +0100

    Dynamic event creation working

--- ToDoUserControl/ToDoTable.Designer.cs
+++ ToDoUserControl/ToDoTable.Designer.cs
@@ -28,52 +28,52 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.button1 = new System.Windows.Forms.Button();
-            this.tableLayoutPanel1.SuspendLayout();
+            this.tlpToDo = new System.Windows.Forms.TableLayoutPanel();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.tlpToDo.SuspendLayout();
             this.SuspendLayout();
             // 
-            // tableLayoutPanel1
+            // tlpToDo
             // 
-            this.tableLayoutPanel1.ColumnCount = 2;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 5.166667F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 94.83334F));
-            this.tableLayoutPanel1.Controls.Add(this.button1, 0, 0);
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
-            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 1;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(600, 500);
-            this.tableLayoutPanel1.TabIndex = 0;
+            this.tlpToDo.ColumnCount = 2;
+            this.tlpToDo.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 5.166667F));
+            this.tlpToDo.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 94.83334F));
+            this.tlpToDo.Controls.Add(this.btnAdd, 0, 0);
+            this.tlpToDo.Location = new System.Drawing.Point(0, 0);
+            this.tlpToDo.Name = "tlpToDo";
+            this.tlpToDo.RowCount = 1;
+            this.tlpToDo.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tlpToDo.Size = new System.Drawing.Size(600, 500);
+            this.tlpToDo.TabIndex = 0;
             // 
-            // button1
+            // btnAdd
             // 
-            this.button1.FlatAppearance.BorderSize = 0;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Image = global::ToDoUserControl.Properties.Resources.add;
-            this.button1.Location = new System.Drawing.Point(0, 0);
-            this.button1.Margin = new System.Windows.Forms.Padding(0);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(30, 23);
-            this.button1.TabIndex = 0;
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.AddRow);
+            this.btnAdd.FlatAppearance.BorderSize = 0;
+            this.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAdd.Image = global::ToDoUserControl.Properties.Resources.add;
+            this.btnAdd.Location = new System.Drawing.Point(0, 0);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(0);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(30, 23);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.AddRow);
             // 
             // ToDoTable
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.tlpToDo);
             this.Name = "ToDoTable";
             this.Size = new System.Drawing.Size(600, 500);
-            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tlpToDo.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TableLayoutPanel tlpToDo;
+        private System.Windows.Forms.Button btnAdd;
     }
 }
--- ToDoUserControl/ToDoTable.cs
+++ ToDoUserControl/ToDoTable.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Drawing;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ToDoUserControl
@@ -19,10 +13,24 @@ namespace ToDoUserControl
 
         private void AddRow(object sender, EventArgs e)
         {
-            tableLayoutPanel1.RowCount = tableLayoutPanel1.RowCount + 1;
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 20));
-            tableLayoutPanel1.Controls.Add(new CheckBox() { Text = "", Margin = new Padding(8, 0, 0, 0) }, 0, tableLayoutPanel1.RowCount - 1);
-            tableLayoutPanel1.Controls.Add(new TextBox() { Text = "", BackColor = SystemColors.Control, BorderStyle = BorderStyle.None }, 1, tableLayoutPanel1.RowCount - 1);
+            tlpToDo.RowCount = tlpToDo.RowCount + 1;
+            tlpToDo.RowStyles.Add(new RowStyle(SizeType.Absolute, 20));
+            tlpToDo.Controls.Add(new CheckBox() { Text = "", Margin = new Padding(8, 0, 0, 0) }, 0, tlpToDo.RowCount - 1);
+            Control c = tlpToDo.GetControlFromPosition(0, tlpToDo.RowCount - 1);
+            c.Click += new EventHandler(evento);
+            tlpToDo.Controls.Add(new TextBox() { Text = "", BackColor = SystemColors.Control, BorderStyle = BorderStyle.None, Dock = DockStyle.Fill }, 1, tlpToDo.RowCount - 1);
+        }
+
+        private void evento(object sender, EventArgs e)
+        {
+            TableLayoutPanelCellPosition tlpcp = tlpToDo.GetPositionFromControl((Control)sender);
+            
+            Control c = tlpToDo.GetControlFromPosition(tlpcp.Column+1, tlpcp.Row);
+
+            if (c.Enabled)
+                c.Enabled = false;
+            else
+                c.Enabled = true;
         }
     }
 }
